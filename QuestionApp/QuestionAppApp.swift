9bacc04085a2b2ddfbd452806ff840daab6702e3
Dagmar <beckeldagmar@gmail.com>
2023-07-14 16:40:26 -0500

    Initial Commit

---
+++ QuestionApp/QuestionAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  QuestionAppApp.swift
+//  QuestionApp
+//
+//  Created by GoldenCalfCompanyMacPro2 on 7/14/23.
+//
+
+import SwiftUI
+
+@main
+struct QuestionAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
