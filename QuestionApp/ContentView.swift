489e314851711e3fbbcce862b9d68950c52b4f2c
Dagmar <beckeldagmar@gmail.com>
2023-07-14 20:37:00 -0500

    PageTwo added

--- QuestionApp/ContentView.swift
+++ QuestionApp/ContentView.swift
@@ -9,8 +9,14 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        NavigationView{
+            VStack{
+                Text("Dagmar Quiz")
+                NavigationLink(destination: PageTwo()){
+                        Text(">")
+                }
+            }
+        }
     }
 }
 
---
+++ QuestionApp/PageTwo.swift
@@ -0,0 +1,20 @@
+//
+//  PageTwo.swift
+//  QuestionApp
+//
+//  Created by GoldenCalfCompanyMacPro2 on 7/14/23.
+//
+
+import SwiftUI
+
+struct PageTwo: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct PageTwo_Previews: PreviewProvider {
+    static var previews: some View {
+        PageTwo()
+    }
+}
